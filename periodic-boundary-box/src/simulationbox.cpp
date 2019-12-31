919a9a9f925f40f55938efbc4324b27b5f058406
kadupitiya <kadupitiya@kadupitiya.lk>
2019-12-31 00:43:28 -0500

    particle initialization changed

--- periodic-boundary-box/src/simulationbox.cpp
+++ periodic-boundary-box/src/simulationbox.cpp
@@ -9,12 +9,18 @@ void SIMULATIONBOX::put_ljatoms_inside(vector<PARTICLE>& ljatom, int number_ljat
   
   //ofstream out_initialize_position("ip.lammpstrj", ios::out);
   cout << "# of atoms created in the box... " << "  ";
+
+  //This is the diameter of a particle
   double a = 1;
   cout << "lattice spacing (based on crystal of closest packing) is " << a << endl;
   
   unsigned int num_atoms_linear = int(ceil(pow((double(number_ljatom)),1.0/3.0)));
   cout << "in each direction, about " << num_atoms_linear << " atoms " << endl;
-  
+
+  //double lx_start = -lx/2, ly_start = -ly/2, lz_start = -lz/2;
+  //(num_atoms_linear-1): -1 is because worst case: fully packed d~1.0 atom center is at the edge of the box
+  double lx_start = -a*(num_atoms_linear-1)/2,  ly_start = -a*(num_atoms_linear-1)/2, lz_start = -a*(num_atoms_linear-1)/2;
+
   for (unsigned int i = 0; i < num_atoms_linear; i++)
   {
     for (unsigned int j = 0; j < num_atoms_linear; j++)
@@ -23,9 +29,9 @@ void SIMULATIONBOX::put_ljatoms_inside(vector<PARTICLE>& ljatom, int number_ljat
       {
 	if (int(ljatom.size()) < number_ljatom)	// stop if atoms created are equal to the requested number
 	{
-	  double x = (-lx/2) + i*a;
-	  double y = (-ly/2) + j*a;
-	  double z = (-lz/2) + k*a;
+	  double x = lx_start + i*a;
+	  double y = ly_start + j*a;
+	  double z = lz_start + k*a;
 	  if (z >= lz/2.0 - a/2.0 || y >= ly/2.0 - a/2.0 || x >= lx/2.0 - a/2.0)
 	    continue;
 	  VECTOR3D position = VECTOR3D(x,y,z);
